419e855a89a65179fb6356310d72d720806cbbac
oneilld136 <oneilld136@gmail.com>
2019-04-20 18:53:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,14 +2,16 @@
 require 'yaml'
 
 def load_library
-  library= YAML.load_file
+  library= YAML.load_file('some.yml')
   # code goes here
 end
 
 def get_japanese_emoticon
+japanese_emoticon=YAML.get_file('some.yml')
   # code goes here
 end
 
 def get_english_meaning
+  english_meaning=YAML.get_file(some.yml)
   # code goes here
 end
